bfa51530b057f23960ff19367a3dbef61c590c7d
pdawwg08 <pdawwg08@yaho.com>
2014-04-10 13:04:04 -0600

    Expression Worksheet

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,32 @@
+// Preston Cain 4/10/2014 Expression Worksheet
+
+//Dog Years
+var age = 5
+dog = age * 7
+console.log("Sparky is " + age + " human years old which is " + dog + " in dog years.")
+
+//Slice of Pie part 1
+var slices = 8
+var people = 11
+var pizzas = 3
+peopleSlices = (pizzas * slices)/people
+console.log("Each person ate " + peopleSlices + " slices of pizza at the party.")
+
+//Slice of Pie part 2
+sparkySlices = (pizzas * slices)%people
+console.log("Sparky got " + sparkySlices + " slices of pizza.")
+
+//Average Shopping Bill
+var grocery = [60,73,102,41,65]
+total =  grocery[0] + grocery[1] + grocery[2] + grocery[3] + grocery[4]
+average = total/5
+console.log("You have spent a total of $"+total+" on groceries over 5 weeks. That is an average of $"+average+" per week.")
+
+//Discounts
+var price = 27
+var discount = 20
+var description = "Turtle"
+var tax = 7.25
+priceWithout = price*(1-(discount/100))
+priceWith = priceWithout*(1+(tax/100))
+console.log("Your "+description+" was originally $"+price+", but after a "+discount+"% discount, it is now $"+priceWithout+" without tax, and $"+priceWith+" with tax.")
