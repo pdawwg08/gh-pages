3c16c52513973b7c4edbcf2c10a852affc5b5b82
pdawwg08 <pdawwg08@yaho.com>
2014-04-17 15:45:36 -0600

    prompts

--- Expressions/wacky/js/script.js
+++ Expressions/wacky/js/script.js
@@ -1,14 +1,17 @@
 // Preston Cain 4/17/2014 Conditionals
 
 //Wacky
+//prompt for slices
 var slices = prompt("We are calculating the number of pies you will need for thanksgiving.\nPlease enter the number of slices per pie:");
 if(slices==""){
 	var slices = prompt("You forgot to input the number of slices per pie!");
 }
+//prompt for guests
 var guests = prompt("Please enter the number of guests:");
 if(guests==""){
 	var guests = prompt("You forgot to input the number of guests!");
 }
+//prompt for fat
 var fat = prompt("Please enter how fat you want to be (from 1 to 10):");
 if(fat==""){
 	var fat = prompt("You forgot to input how fat you want to be (from 1 to 10)!");
