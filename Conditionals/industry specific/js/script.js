16ee9e75af27ad1b663f8fb860d56106b2aa6b66
pdawwg08 <pdawwg08@yaho.com>
2014-04-17 15:27:59 -0600

    fixing megapixel issue

--- Conditionals/industry specific/js/script.js
+++ Conditionals/industry specific/js/script.js
@@ -5,9 +5,9 @@ var dpi = prompt("We are calculating the pixels in the desired image.\nPlease en
 var width = prompt("Please enter the width:");
 var height = prompt("Please enter the height:");
 var pixels = (width*dpi) * (height*dpi);
-if(pixels >= 3000000000){
+if(pixels >= 3000000){
 	result = "You need at least a 3 Megapixel camera to capture this image.";
-}else if(pixels >= 1000000000){
+}else if(pixels >= 1000000){
 	result = "You only need a 1 Megapixel camera to capture this image.";
 }else{
 	result = "You don't need to worry about the resolution of the camera.\nThe image is less than 1 Megapixel";
