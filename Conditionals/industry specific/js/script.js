f0c8b5732cc51ecdae55cc9042e289fe7d553997
pdawwg08 <pdawwg08@yaho.com>
2014-04-17 15:25:19 -0600

    changing prompt and alert

--- Conditionals/industry specific/js/script.js
+++ Conditionals/industry specific/js/script.js
@@ -1,11 +1,11 @@
 // Preston Cain 4/17/2014 Conditionals
 
 //Industry Specific
-var dpi = prompt("We are calculating the pixels in an image.\nPlease enter the dpi:")
+var dpi = prompt("We are calculating the pixels in the desired image.\nPlease enter the dpi:")
 var width = prompt("Please enter the width:")
 var height = prompt("Please enter the height:")
 var pixels = (width*dpi) * (height*dpi)
 if(pixels >= 3000000000){
-result = "You need at least a 3 Megapixel camera
+result = "You need at least a 3 Megapixel camera to capture this image."
 alert(result)
 console.log(result)
