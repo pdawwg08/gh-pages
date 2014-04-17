293fbcd5032b009b858077f4920f13a964d01554
pdawwg08 <pdawwg08@yaho.com>
2014-04-17 15:21:33 -0600

    semi-colons

--- Expressions/wacky/js/script.js
+++ Expressions/wacky/js/script.js
@@ -1,28 +1,28 @@
 // Preston Cain 4/17/2014 Conditionals
 
 //Wacky
-var slices = prompt("We are calculating the number of pies you will need for thanksgiving.\nPlease enter the number of slices per pie:")
+var slices = prompt("We are calculating the number of pies you will need for thanksgiving.\nPlease enter the number of slices per pie:");
 if(slices==""){
 	var slices = prompt("You forgot to input the number of slices per pie!");
 }
-var guests = prompt("Please enter the number of guests:")
+var guests = prompt("Please enter the number of guests:");
 if(guests==""){
 	var guests = prompt("You forgot to input the number of guests!");
 }
-var fat = prompt("Please enter how fat you want to be (from 1 to 10):")
+var fat = prompt("Please enter how fat you want to be (from 1 to 10):");
 if(fat==""){
 	var fat = prompt("You forgot to input how fat you want to be (from 1 to 10)!");
 }
 if(fat > 10){
-	alert("You must really enjoy thanksgiving!")
+	alert("You must really enjoy thanksgiving!");
 }
 if(fat < 1){
-	alert("You must really hate pie!")
+	alert("You must really hate pie!");
 }
 if(slices=="" || guests==""){
-	alert("You did not answer all questions.")
+	alert("You did not answer all questions.");
 }
-var pies = (guests*fat)/slices
-result = "The number of pies you will need is "+pies
-alert(result)
-console.log(result)
+var pies = (guests*fat)/slices;
+result = "The number of pies you will need is "+pies;
+alert(result);
+console.log(result);
