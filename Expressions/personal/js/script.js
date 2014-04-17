6e7ef32fbb7fd5a98265975026000908e74af0a2
pdawwg08 <pdawwg08@yaho.com>
2014-04-17 15:42:28 -0600

    error prevention comments

--- Expressions/personal/js/script.js
+++ Expressions/personal/js/script.js
@@ -3,15 +3,18 @@
 //Personal
 //prompt for diapers
 var diaperDaily = prompt("We are calculating how many diaper boxes you will use in a year.\nPlease enter the number of diapers you use per day:");
+//prevent errors
 if(diaperDaily==""){
 	var diaperDaily = prompt("You forgot to input the number of diapers you use per day!");
 }
 //prompt for diapers in a box
 var box = prompt("Please enter the number of diapers are in a box of your favorite brand:");
+//prevent errors
 if(box==""){
 	var box = prompt("You forgot to input the number of diapers in a box!");
 }
 var diaperTotal = diaperDaily*365;
+//prevent errors
 if(diaperDaily==0 || box==0){
 	var diaperBoxes=0;
 }else{
