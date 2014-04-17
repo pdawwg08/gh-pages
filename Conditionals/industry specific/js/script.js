e1231bf7d0eee71e30af3c437c938fcbe472dafd
pdawwg08 <pdawwg08@yaho.com>
2014-04-17 15:27:26 -0600

    semi-colons

--- Conditionals/industry specific/js/script.js
+++ Conditionals/industry specific/js/script.js
@@ -1,15 +1,16 @@
 // Preston Cain 4/17/2014 Conditionals
 
 //Industry Specific
-var dpi = prompt("We are calculating the pixels in the desired image.\nPlease enter the dpi:")
-var width = prompt("Please enter the width:")
-var height = prompt("Please enter the height:")
-var pixels = (width*dpi) * (height*dpi)
+var dpi = prompt("We are calculating the pixels in the desired image.\nPlease enter the dpi:");
+var width = prompt("Please enter the width:");
+var height = prompt("Please enter the height:");
+var pixels = (width*dpi) * (height*dpi);
 if(pixels >= 3000000000){
-	result = "You need at least a 3 Megapixel camera to capture this image."
-}else if(pixels >=1000000000){
-	result = "You only need a 1 Megapixel camera to capture this image."
+	result = "You need at least a 3 Megapixel camera to capture this image.";
+}else if(pixels >= 1000000000){
+	result = "You only need a 1 Megapixel camera to capture this image.";
 }else{
-	result = "You don't need to worry about the resolution of the camera.\nThe image is less than 1 Megapixel"
-alert(result)
-console.log(result)
+	result = "You don't need to worry about the resolution of the camera.\nThe image is less than 1 Megapixel";
+}
+alert(result);
+console.log(result);
