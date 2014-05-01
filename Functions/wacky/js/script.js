cb7a6a7c8a7d24bbdb44dd969e1a73cc12c35d64
pdawwg08 <pdawwg08@yaho.com>
2014-05-01 12:21:30 -0600

    wacky
    simplified the prompts for the wacky using functions

--- Functions/wacky/js/script.js
+++ Functions/wacky/js/script.js
@@ -1,24 +1,23 @@
-// Preston Cain 4/17/2014 Conditionals
+// Preston Cain 5/1/2014 Functions
 
 //Wacky
+var copyPrompt = function(measurement){
+    var unit = prompt("We are calculating the number of pies you will need for thanksgiving.\nPlease enter "+measurement+":");
+    if(unit==""){
+	var unit = prompt("You forgot to input "+measurement+"!");
+}
+    return unit;
+}
+
 //prompt for slices
-var slices = prompt("We are calculating the number of pies you will need for thanksgiving.\nPlease enter the number of slices per pie:");
-//error prevention
-if(slices==""){
-	var slices = prompt("You forgot to input the number of slices per pie!");
-}
+var slices = copyPrompt("the number of slices per pie");
+
 //prompt for guests
-var guests = prompt("Please enter the number of guests:");
-//error prevention
-if(guests==""){
-	var guests = prompt("You forgot to input the number of guests!");
-}
+var guests = copyPrompt("the number of guests");
+
 //prompt for fat
-var fat = prompt("Please enter how fat you want to be (from 1 to 10):");
-//error prevention
-if(fat==""){
-	var fat = prompt("You forgot to input how fat you want to be (from 1 to 10)!");
-}
+var fat = copyPrompt("how fat you want to be (from 1 to 10)");
+
 //humorous alert
 if(fat > 10){
 	alert("You must really enjoy thanksgiving!");
