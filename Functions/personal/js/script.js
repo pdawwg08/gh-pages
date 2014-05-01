8543892198e76d117a57938b161f8b3a6717bf2a
pdawwg08 <pdawwg08@yaho.com>
2014-05-01 12:29:59 -0600

    comments

--- Functions/personal/js/script.js
+++ Functions/personal/js/script.js
@@ -3,11 +3,11 @@
 //Personal
 
 var copyPrompt = function(measurement){
-    var unit = prompt("We are calculating how many diaper boxes you will use in a year.\nPlease enter the number of "+measurement+":");
+    var unit = prompt("We are calculating how many diaper boxes you will use in a year.\nPlease enter the number of "+measurement+":"); //generic prompt
     if(unit==""){
-	var unit = prompt("You forgot to input the "+measurement+"!");
-}
-    return unit;
+	var unit = prompt("You forgot to input the "+measurement+"!"); //alert
+} //if first prompt is empty
+    return unit; //return what the user put in the prompt
 }
 
 //prompt for diapers
