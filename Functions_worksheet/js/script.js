fc58779fcb0fe0bdce3424c0f27e307b305acca6
pdawwg08 <pdawwg08@yaho.com>
2014-05-01 11:39:59 -0600

    stung!

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -5,8 +5,16 @@ var calcCircum = function(radius){ //create the function
     var circumference = 2*3.14*radius; //calculate the circumference
     return circumference; //return the answer
     }
-var circum = calcCircum(4) //call the function
+var circum = calcCircum(4); //call the function
 console.log("The circumference of the circle is "+circum) //log it
+
+//Stung!
+var stingers = function(weight){ //create the function
+    var stings = weight*8.666666667; //calculate the stings
+    return stings; //return the answer
+}
+var killing = stingers(135); //call the function
+console.log("It takes "+killing+" bee stings to kill this animal") //log it
 
 //Stuff your face
 weight = 275;
