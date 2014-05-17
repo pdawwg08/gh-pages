04419a30893bd7314a4eabc9320efeb66b127491
pdawwg08 <pdawwg08@yaho.com>
2014-05-16 23:27:14 -0600

    variables

--- wk1/goal1_assign_duel1/js/main.js
+++ wk1/goal1_assign_duel1/js/main.js
@@ -5,4 +5,15 @@ Assignment: Duel
 */
 (function(){
 console.log(FIGHT!!)
+    //player name
+    var player1Name = "Spiderman";
+    var player2Name = "Batman";
+    
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+    
+    //player health
+    var player1Health = 100;
+    var player2Health = 100;
 })();
