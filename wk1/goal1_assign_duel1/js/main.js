3c00678da7e88c284c5f036d1f3cde9006581823
pdawwg08 <pdawwg08@yaho.com>
2014-05-17 00:33:39 -0600

    change names and finish fight app

--- wk1/goal1_assign_duel1/js/main.js
+++ wk1/goal1_assign_duel1/js/main.js
@@ -4,42 +4,68 @@ Date:5/16/2014
 Assignment: Duel
 */
 (function(){
-console.log(FIGHT!!)
+
+    console.log("FIGHT!!!");
+
     //player name
-    var player1Name = "Spiderman";
-    var player2Name = "Batman";
-    
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Batman";
+
     //player damage
     var player1Damage = 20;
     var player2Damage = 20;
-    
+
     //player health
-    var player1Health = 100;
-    var player2Health = 100;
-    
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+    //initiate round
+    var round=0;
+
     function fight(){
-    alert(player1Name+":"+player1Health+" *START* "+player2Name":"+player2Health)
-        for (var i=0, i < 10; i++){
-            var minDamage1 = player1Damage *.5;
-            var minDamage2 = player2Damage *.5;
-            var f1 = Math.floor(Math.random() * (player1Damage - minDamage1) + minDamage1);
-            var f2 = Math.floor(Math.random() * (player2Damage - minDamage2) + minDamage2);
-            player1Health-=f1;
-            player2Health-=f2;
-            alert(player1Name+":"+player1Health+" *START* "+player2Name":"+player2Health)
-            var results = winnerCheck();
-        }
-    }
+        alert(playerOneName+":"+playerOneHealth+"  *START*  "+playerTwoName+":"+playerTwoHealth);
+        for (var i = 0; i < 10; i++)
+        {
+            var minDamage1 = player1Damage * .5;
+            var minDamage2 = player2Damage * .5;
+            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+            //inflict damage
+            playerOneHealth-=f1;
+            playerTwoHealth-=f2;
+
+            console.log(playerOneName+": "+playerOneHealth + " " + playerTwoName+":"+playerTwoHealth);
+
+            //check for victor
+            var result = winnerCheck();
+            console.log(result);
+            if (result==="no winner")
+            {
+                round++;
+                alert(playerOneName+":"+playerOneHealth+"  *ROUND "+round+" OVER"+"*  "+playerTwoName+":"+playerTwoHealth);
+
+            } else{
+                alert(result);
+                break;
+            };
+
+          };
+    };
+
     function winnerCheck(){
-        var result="no winner"
-        if(player1Health<1 && player2Health<1{
-           result = "You Both Die";
-           }else if(player1Health<1{
-           result = player2Name+" Wins!";
-           }else if(player2Health<1){
-            result = player1Name+" Wins!";
-        }
-        return = result
-    }
+        var result="no winner";
+        if (playerOneHealth<1 && playerTwoHealth<1)
+        {
+            result = "You Both Die";
+        } else if(playerOneHealth<1){
+            result =playerTwoName+" WINS!!!"
+        } else if (playerTwoHealth<1)
+        {
+            result = playerOneName+" WINS!!!"
+        };
+       return result;
+    };
     fight();
+
 })();
