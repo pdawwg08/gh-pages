51ee05249b35567cdfbae3cf8a070c08248c46c0
pdawwg08 <pdawwg08@yaho.com>
2014-05-16 22:31:30 -0600

    console logs

--- wk2/goal4_assign_guess/js/main.js
+++ wk2/goal4_assign_guess/js/main.js
@@ -9,8 +9,10 @@
 
 //Game variables
 var guess=Math.round(Math.random()*(9)+1);
+console.log(guess)
 var dom = {
     input: document.querySelector("#input"),
     output: document.querySelector("#output"),
     button: document.querySelector("button")
 }
+console.log(dom.output.innerHTML)
