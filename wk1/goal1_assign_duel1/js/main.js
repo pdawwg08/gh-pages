4226cec99805a889067a7deee0fb5405db4e7586
pdawwg08 <pdawwg08@yaho.com>
2014-05-16 23:35:24 -0600

    calculate damage

--- wk1/goal1_assign_duel1/js/main.js
+++ wk1/goal1_assign_duel1/js/main.js
@@ -20,6 +20,12 @@ console.log(FIGHT!!)
     function fight(){
     alert(player1Name+":"+player1Health+" *START* "+player2Name":"+player2Health)
     for (var i=0, i < 10; i++){
+        var minDamage1 = player1Damage *.5;
+        var minDamage2 = player2Damage *.5;
+        var f1 = Math.floor(Math.random() * (player1Damage - minDamage1) + minDamage1);
+        var f2 = Math.floor(Math.random() * (player2Damage - minDamage2) + minDamage2);
+        player1Health-=f1;
+        player2Health-=f2;
     }
     winnerCheck();
     }
