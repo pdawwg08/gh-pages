c81314044e00d76ef6eb08f896262c4f7b078176
pdawwg08 <pdawwg08@yaho.com>
2014-05-23 15:36:53 -0600

    week 3 files

---
+++ wk3/goal4_assign_duel3/js/main.js
@@ -0,0 +1,62 @@
+/*
+Name:Preston Cain
+Date:5/23/2014
+Assignment: Duel III
+*/
+(function(){
+
+    console.log("FIGHT!!!");
+
+    var playerOne = ["Spiderman",20,100]
+    var playerTwo = ["Batman",20,100]
+
+    //initiate round
+    var round=0;
+
+    function fight(){
+        alert(playerOne[0]+":"+playerOne[2]+"  *START*  "+playerTwo[0]+":"+playerTwo[2]);
+        for (var i = 0; i < 10; i++)
+        {
+            var minDamage1 = playerOne[1] * .5;
+            var minDamage2 = playerOne[1] * .5;
+            var f1 = Math.floor(Math.random()*(playerOne[1]-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(playerOne[1]-minDamage2)+minDamage2);
+
+            //inflict damage
+            playerOne[2]-=f1;
+            playerTwo[2]-=f2;
+
+            console.log(playerOne[0]+": "+playerOne[2] + " " + playerTwo[0]+":"+playerTwo[2]);
+
+            //check for victor
+            var result = winnerCheck();
+            console.log(result);
+            if (result==="no winner")
+            {
+                round++;
+                alert(playerOne[0]+":"+playerOne[2]+"  *ROUND "+round+" OVER"+"*  "+playerTwo[0]+":"+playerTwo[2]);
+
+            } else{
+                alert(result);
+                break;
+            };
+
+          };
+    };
+
+    function winnerCheck(){
+        var result="no winner";
+        if (playerOne[2]<1 && playerTwo[2]<1)
+        {
+            result = "You Both Die";
+        } else if(playerOne[2]<1){
+            result =playerTwo[0]+" WINS!!!"
+        } else if (playerTwo[2]<1)
+        {
+            result = playerOne[0]+" WINS!!!"
+        };
+       return result;
+    };
+    fight();
+
+})();
