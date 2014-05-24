601563f3062c3ae7bae554d918a600e97f5d183e
pdawwg08 <pdawwg08@yaho.com>
2014-05-23 22:42:43 -0600

    validation names

---
+++ wk3/goal5_assign_validation/js/validate.js
@@ -0,0 +1,65 @@
+/*
+     Developed by the JavaScript team at Fullsail
+     Date: 1306
+*/
+
+(function(){
+
+    myform.onsubmit = function(e){
+
+        //Below is one example of the validateField call with an argument.
+        //You must dynamically retrieve the ID name from the DOM/HTML.
+
+        validateField("f_username");  //id = is the form input field ID
+        validateField("f_email");
+        validateField("f_phone");
+        validateField("f_password");
+        validateField("f_ssn");
+
+        e.preventDefault();
+        return false;
+    };
+
+
+    var validateField = function(inputName){
+
+        if (inputName.name === "f_username"){
+            var pattern = 'a RegEx pattern goes here'
+
+        };
+        if (inputName.name === "f_email"){
+            var pattern = 'a RegEx pattern goes here'
+
+        };
+        if (inputName.name === "f_phone"){
+            var pattern = 'a RegEx pattern goes here'
+
+        };
+        if (inputName.name === "f_password"){
+            var pattern = 'a RegEx pattern goes here'
+
+        };
+        if (inputName.name === "f_ssn"){
+            var pattern = 'a RegEx pattern goes here'
+
+        };
+
+        var pass = 'the RegEx .test statement is needed here';
+        var errorMsg = inputName.nextSibling.nextSibling.nextSibling.nextSibling;
+
+        if (!pass || inputName.value.length < 2){
+            errorMsg.style.display='block';
+            inputName.style.backgroundColor = 'red';
+        } else if (pass && inputName.value.length > 5){
+            errorMsg.style.display='none';
+            inputName.style.backgroundColor = 'green';
+        } else {
+            errorMsg.style.display='none';
+            inputName.style.backgroundColor = 'white';
+        };
+    };
+
+})();  // end wrapper
+
+
+
