b5b5d02e2aa8c208976ecf57ebff2fdcd0e996f6
pdawwg08 <pdawwg08@yaho.com>
2014-05-23 21:21:41 -0600

    remove loop

--- wk3/goal4_assign_duel3/js/main.js
+++ wk3/goal4_assign_duel3/js/main.js
@@ -17,41 +17,35 @@ Assignment: Duel III
         document.getElementById("kabal").innerHTML = playerOne[0]+":"+playerOne[2];
         document.getElementById("kratos").innerHTML = playerTwo[0]+":"+playerTwo[2];
         document.getElementsByTagName("input")[0].value = "START";
-        alert(playerOne[0]+":"+playerOne[2]+"  *START*  "+playerTwo[0]+":"+playerTwo[2]);
-        for (var i = 0; i < 10; i++)
+        
+        var minDamage1 = playerOne[1] * .5;
+        var minDamage2 = playerOne[1] * .5;
+        var f1 = Math.floor(Math.random()*(playerOne[1]-minDamage1)+minDamage1);
+        var f2 = Math.floor(Math.random()*(playerOne[1]-minDamage2)+minDamage2);
+
+        //inflict damage
+        playerOne[2]-=f1;
+        playerTwo[2]-=f2;
+
+        console.log(playerOne[0]+": "+playerOne[2] + " " + playerTwo[0]+":"+playerTwo[2]);
+
+        //check for victor
+        var result = winnerCheck();
+        console.log(result);
+        if (result==="no winner")
         {
-            var minDamage1 = playerOne[1] * .5;
-            var minDamage2 = playerOne[1] * .5;
-            var f1 = Math.floor(Math.random()*(playerOne[1]-minDamage1)+minDamage1);
-            var f2 = Math.floor(Math.random()*(playerOne[1]-minDamage2)+minDamage2);
+            round++;
+            document.getElementById("kabal").innerHTML = playerOne[0]+":"+playerOne[2];
+            document.getElementById("kratos").innerHTML = playerTwo[0]+":"+playerTwo[2];
+            document.getElementsByTagName("input")[0].value = "ROUND "+round+" OVER";
 
-            //inflict damage
-            playerOne[2]-=f1;
-            playerTwo[2]-=f2;
-
-            console.log(playerOne[0]+": "+playerOne[2] + " " + playerTwo[0]+":"+playerTwo[2]);
-
-            //check for victor
-            var result = winnerCheck();
-            console.log(result);
-            if (result==="no winner")
-            {
-                round++;
-                document.getElementById("kabal").innerHTML = playerOne[0]+":"+playerOne[2];
-                document.getElementById("kratos").innerHTML = playerTwo[0]+":"+playerTwo[2];
-                document.getElementsByTagName("input")[0].value = "ROUND "+round+" OVER";
-                alert(playerOne[0]+":"+playerOne[2]+"  *ROUND "+round+" OVER"+"*  "+playerTwo[0]+":"+playerTwo[2]);
-
-            } else{
-                document.getElementById("kabal").innerHTML = "";
-                document.getElementById("kratos").innerHTML = "";
-                document.getElementsByTagName("input")[0].value = result;
-                break;
-            };
-
-          };
-    };
-
+        } else{
+            document.getElementById("kabal").innerHTML = "";
+            document.getElementById("kratos").innerHTML = "";
+            document.getElementsByTagName("input")[0].value = result;
+        };
+        return false;
+    }
     function winnerCheck(){
         var result="no winner";
         if (playerOne[2]<1 && playerTwo[2]<1)
