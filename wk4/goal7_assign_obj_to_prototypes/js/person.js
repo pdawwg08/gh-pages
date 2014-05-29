483560b2017ef81265b8f56bdc8a66e34468cbe1
pdawwg08 <pdawwg08@yaho.com>
2014-05-29 01:40:28 -0600

    fix jobs

--- wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -12,7 +12,7 @@
         console.log("Person Created :"+name);
         this.name = name;
         this.action = Person.actions[Math.floor(Math.random()*Person.actions.length)];
-        this.job = Person.jobs[Math.floor(Math.random()*Person.actions.length)];
+        this.job = Person.jobs[Math.floor(Math.random()*Person.jobs.length)];
         this.row = row;
         document.getElementById("r"+this.row+"c3").innerHTML = this.action;
     }
