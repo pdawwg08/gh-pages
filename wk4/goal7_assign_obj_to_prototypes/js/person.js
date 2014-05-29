f3a4daba3f43dcc0c54d3eebe632856b1970d216
pdawwg08 <pdawwg08@yaho.com>
2014-05-29 01:28:40 -0600

    start person

--- wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -3,3 +3,8 @@
  * Class: PWA
  * Goal: Goal7
  */
+(function(){
+    window.Person = Person;
+    Person.jobs = ["astronaut","chef","designer","athlete"];
+    Person.actions = ["thinking","forgetting","remembering"]
+})();
